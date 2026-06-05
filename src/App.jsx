446f6e957494dfd48a8266b5ea3699f27c981b58
braiancalpar <braian.santos@calpar.com.br>
2026-06-05 13:51:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { Aside } from "./components/Aside";
+import Card from "./components/Card";
+import { Container } from "./components/Container";
+import { DailyBudget } from "./components/DailyBudget";
+import { Main } from "./components/Main";
+import { SavingsStatus } from "./components/SavingsStatus";
+import { SearchInput } from "./components/SearchInput";
+import { Typography } from "./components/Typography";
+
+import styles from "./app.module.css";
+
+function App() {
+  return (
+    <Container>
+      <Aside />
+      <Main>
+        <div className={styles.container}>
+          <SearchInput />
+          <div>
+            <Typography variant="h1">Olá, Kaladin!</Typography>
+            <Typography variant="body">
+              Veja como estão suas finanças hoje.
+            </Typography>
+          </div>
+          <section className={styles.grid}>
+            <Card>
+              <Card.Header>Orçamento diário disponível:</Card.Header>
+              <Card.Body>
+                <DailyBudget value={200} />
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Progresso da meta financeira:</Card.Header>
+              <Card.Body>
+                <SavingsStatus percent={40} />
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Orçamento diário disponível:</Card.Header>
+              <Card.Body>R$ 200</Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Orçamento diário disponível:</Card.Header>
+              <Card.Body>
+                <p>R$ 200</p>
+                <p>R$ 200</p>
+                <p>R$ 200</p>
+                <p>R$ 200</p>
+                <p>R$ 200</p>
+                <p>R$ 200</p>
+                <p>R$ 200</p>
+              </Card.Body>
+            </Card>
+          </section>
+        </div>
+      </Main>
+    </Container>
+  );
+}
+
+export default App;
